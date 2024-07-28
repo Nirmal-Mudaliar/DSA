3d2b8433ba779d332b4b2f5cc74282aa1c406db4
Nirmal Mudaliar <Nirmal Mudaliar@DESKTOP-OKKKOBD>
2024-07-28 18:20:48 +0530

    feat(src:all-subsets): add optimal approach to print all subsets problem

---
+++ src/recursion/sumOfAllSubSets/optimalApproach/AllSubSets.java
@@ -0,0 +1,23 @@
+package recursion.sumOfAllSubSets.optimalApproach;
+
+import java.util.ArrayList;
+
+public class AllSubSets {
+    private void getAllSubsets(int index, ArrayList<Integer> arr, ArrayList<Integer> currList, ArrayList<ArrayList<Integer>> ans) {
+        if (index == arr.size()) {
+            ans.add(new ArrayList<>(currList));
+            return;
+        }
+        currList.add(arr.get(index));
+        getAllSubsets(index + 1, arr, currList, ans);
+        currList.removeLast();
+        getAllSubsets(index + 1, arr, currList, ans);
+    }
+
+    public ArrayList<ArrayList<Integer>> calculateAllSubsets(ArrayList<Integer> arr) {
+        ArrayList<Integer> currentList = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        getAllSubsets(0, arr, currentList, ans);
+        return ans;
+    }
+}
